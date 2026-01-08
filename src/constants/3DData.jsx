d9f966c9220d4f9afccf0386c573ab0ce17273b4
ismoilrz <ismoil4766451155@gmail.com>
2026-01-08 13:36:03 +0300

    data

--- src/constants/3DData.jsx
+++ src/constants/3DData.jsx
@@ -4,6 +4,7 @@ import dio3 from "../assets/dio3.svg";
 
 export const Data3d = [
      {
+    key: 1,
     name: "BMW M8",
     frmes: [
       "https://bmwyedek.com/image/catalog/BMW/BMW%20Seriler/Bmw%20M%20Serisi/BMW%20M8%20Coupe%20xDrive%20Competition/bmw_m8_competition_bmwyedek.png",
@@ -65,4 +66,580 @@ export const Data3d = [
       spendPerUnit: "$811",
     },
   },
+
+     {
+    key: 2,
+    name: "LAMBORGHINI URUS",
+    frmes: [
+      "https://static.vecteezy.com/system/resources/previews/048/028/417/non_2x/lamborghini-urus-suv-on-isolated-transparent-background-png.png",
+      "https://cloud01.topsite.nl/lamborghini.stgallen/car/27/silhouette-menu-2234.png",
+      "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=scale-down,width=1280/sites/default/files/article_images/urus-02.jpg?itok=w1B8CBAh",
+      "https://auto.vercity.ru/gallery/img/automobiles/Lamborghini/2019-11%20Lamborghini%20Urus%20by%20Mansory/900x/1575191923_85.jpg",
+      "https://larte-design.com/storage/app/media/kits/lamborghini/urus-se/coming-soon/renders/grey-lambo-urus-se-facelift-rear.webp",
+      "https://images.dealer.com/ddc/vehicles/2025/Lamborghini/Urus/SUV/trim_SE_59705f/perspective/side-left/2025_76.png",
+      "https://larte-design.ru/storage/app/media/kits/lamborghini/urus-s/colors/grigio-nimbus/lamborgnini-urus-carbon-upgrade-grigio-nimbus.webp",
+      "https://cdna.artstation.com/p/marketplace/presentation_assets/004/213/754/large/file.jpg?1736692421",
+    ],
+    weeklyMileage: [
+      {
+        day: "Mon",
+        km: 26 
+      },
+      {
+        day: "Tue",
+        km: 21 
+      },
+      {
+        day: "Wed",
+        km: 8 
+      },
+      {
+        day: "Thu",
+        km: 17 
+      },
+      {
+        day: "Fri",
+        km: 23 
+      },
+      {
+        day: "Sat",
+        km: 31 
+      },
+      {
+        day: "Sun",
+        km: 29 
+      },
+    ],
+     marketing: {
+      user: "John Junior",
+      averagePrice: "$227,403",
+      marketAverage: "$224,302",
+      excellentImpressionShare: "62%",
+      excellentImpressionShareV: 62,
+      desDioValue1: 84,
+      desDioValue2: 60,
+      desDioValue3: 73,
+      decisionValue: "$908",
+      modelSpend: {
+        src1: dio1,
+        src2: dio2,
+        src3: dio3,
+        blue: "$1,480",
+        red: "$1,290",
+      },
+      spendPerUnit: "$908",
+    },
+  },
+
+     {
+    key: 3,
+    name: "MERCEDES-BENZ AMG GTR",
+    frmes: [
+      "https://i.pinimg.com/originals/20/cc/f1/20ccf199c3a66677eae4f8ce39bc61d7.png",
+      "https://i.pinimg.com/originals/ca/76/de/ca76de0d8929ad3e6e267649c146930c.png",
+      "https://www.renderhub.com/zifir3d/mercedes-benz-amg-gt-coupe-2024/mercedes-benz-amg-gt-coupe-2024-25.jpg",
+      "https://www.renderhub.com/squir/mercedes-amg-gt-r-pro-2020/mercedes-amg-gt-r-pro-2020-05.jpg",
+      "https://www.renderhub.com/zifir3d/mercedes-benz-amg-gt-coupe-2024/mercedes-benz-amg-gt-coupe-2024-07.jpg",
+      "https://www.renderhub.com/lowmod/mercedes-benz-amg-gt-coupe-2024/mercedes-benz-amg-gt-coupe-2024-04.jpg",
+      "https://file.kelleybluebookimages.com/kbb/base/evox/CP/12097/2018-Mercedes-Benz-Mercedes-AMG%20GT-front_12097_032_1822x708_297_cropped.png",
+      "https://assets.meinauto.de/image/upload/q_auto:eco/f_auto/dpr_1.0/c_scale,h_614,w_920/v1/prod/mercedes/amggt/1/2roadster/mercedes_18amggtroadsterrs1e_frontview.png",
+    ],
+    weeklyMileage: [
+      {
+        day: "Mon",
+        km: 27 
+      },
+      {
+        day: "Tue",
+        km: 17 
+      },
+      {
+        day: "Wed",
+        km: 14 
+      },
+      {
+        day: "Thu",
+        km: 17 
+      },
+      {
+        day: "Fri",
+        km: 28 
+      },
+      {
+        day: "Sat",
+        km: 34 
+      },
+      {
+        day: "Sun",
+        km: 31 
+      },
+    ],
+     marketing: {
+      user: "Bill Martin",
+      averagePrice: "$93,873",
+      marketAverage: "$88,508",
+      excellentImpressionShare: "42%",
+      excellentImpressionShareV: 42,
+      desDioValue1: 71,
+      desDioValue2: 77,
+      desDioValue3: 45,
+      decisionValue: "$843",
+      modelSpend: {
+        src1: dio1,
+        src2: dio2,
+        src3: dio3,
+        blue: "$1,020",
+        red: "$1,005",
+      },
+      spendPerUnit: "$843",
+    },
+  },
+
+     {
+    key: 4,
+    name: "AUDI RS6",
+    frmes: [
+      "https://p.turbosquid.com/ts-thumb/no/TUgCLN/B0/3/png/1712139361/1920x1080/fit_q87/c95eb67b70e5b0edb6961678144c45c11b797043/3.jpg",
+      "https://aristocar.be/wp-content/uploads/2019/01/audi_rs6.png",
+      "https://files.hodoor.world/main/f7086efb-a736-4c24-aea2-7b2f0768fd90.png",
+      "https://avatars.mds.yandex.net/get-autoru-vos/5231844/dc97171666d2ee7bdba690bb1a25932a/1200x900",
+      "https://elitecarsale.com/wp-content/uploads/2025/03/snimek-obrazovky-2021-08-20-v-1.webp",
+      "https://i.pinimg.com/736x/6d/b0/f9/6db0f967b8b45b92c2acfea6840f054b.jpg",
+      "https://files.hodoor.world/main/81cdad61-8755-4033-950e-5b6bc079bc0b.png",
+      "https://avatars.mds.yandex.net/get-autoru-vos/17038387/71a7458e4b52ca6c1d456b8dae6fc10f/1200x900",
+    ],
+    weeklyMileage: [
+      {
+        day: "Mon",
+        km: 18 
+      },
+      {
+        day: "Tue",
+        km: 22 
+      },
+      {
+        day: "Wed",
+        km: 11 
+      },
+      {
+        day: "Thu",
+        km: 17 
+      },
+      {
+        day: "Fri",
+        km: 24 
+      },
+      {
+        day: "Sat",
+        km: 16 
+      },
+      {
+        day: "Sun",
+        km: 32 
+      },
+    ],
+     marketing: {
+      user: "Joul Parking",
+      averagePrice: "$137,381",
+      marketAverage: "$132,844",
+      excellentImpressionShare: "58%",
+      excellentImpressionShareV: 58,
+      desDioValue1: 88,
+      desDioValue2: 62,
+      desDioValue3: 89,
+      decisionValue: "$643",
+      modelSpend: {
+        src1: dio1,
+        src2: dio2,
+        src3: dio3,
+        blue: "$1,057",
+        red: "$1,023",
+      },
+      spendPerUnit: "$643",
+    },
+  },
+
+     {
+    key: 5,
+    name: "PORSCHE 911 GT3",
+    frmes: [
+      "https://www.renderhub.com/zifir3d/porsche-911-gt3/porsche-911-gt3-04.jpg",
+      "https://p.turbosquid.com/ts-thumb/tf/l2NR3o/dw/porsche_911_gt3_2022_0011/jpg/1615124778/1920x1080/fit_q87/136e476ee0fc2a69038cbfda71cc90c71a37c38d/porsche_911_gt3_2022_0011.jpg",
+      "https://assets.superhivemarket.com/store/productimage/657860/image/b77bbd813c6770cced49362477da17a7.jpg",
+      "https://p.turbosquid.com/ts-thumb/tf/l2NR3o/vQ/porsche_911_gt3_2022_doors_0004/jpg/1615124813/1920x1080/fit_q87/7bee0a76e49931574cb6006a863cca8b9fddde1b/porsche_911_gt3_2022_doors_0004.jpg",
+      "https://www.renderhub.com/zifir3d/porsche-911-gt3/porsche-911-gt3-09.jpg",
+      "https://bucket.carmodel.com/images/cm-lg/167078_2-1.jpg",
+      "https://www.autogiz.com/product_images/jpeg/Porsche-911-GT3-Touring-2022.jpg",
+      "https://assets.superhivemarket.com/store/productimage/657859/image/9752c942506e259ff4b2cf430e06143e.jpg",
+    ],
+    weeklyMileage: [
+      {
+        day: "Mon",
+        km: 22 
+      },
+      {
+        day: "Tue",
+        km: 15 
+      },
+      {
+        day: "Wed",
+        km: 17 
+      },
+      {
+        day: "Thu",
+        km: 24 
+      },
+      {
+        day: "Fri",
+        km: 29 
+      },
+      {
+        day: "Sat",
+        km: 18 
+      },
+      {
+        day: "Sun",
+        km: 34 
+      },
+    ],
+     marketing: {
+      user: "Brad Tomas",
+      averagePrice: "$182,458",
+      marketAverage: "$181,322",
+      excellentImpressionShare: "38%",
+      excellentImpressionShareV: 38,
+      desDioValue1: 61,
+      desDioValue2: 73,
+      desDioValue3: 47,
+      decisionValue: "$476",
+      modelSpend: {
+        src1: dio1,
+        src2: dio2,
+        src3: dio3,
+        blue: "$832",
+        red: "$654",
+      },
+      spendPerUnit: "$476",
+    },
+  },
+     {
+    key: 6,
+    name: "BMW M8",
+    frmes: [
+      "https://bmwyedek.com/image/catalog/BMW/BMW%20Seriler/Bmw%20M%20Serisi/BMW%20M8%20Coupe%20xDrive%20Competition/bmw_m8_competition_bmwyedek.png",
+      "https://i.pinimg.com/originals/18/8d/18/188d18a54ae795b3242bfe07587964d6.jpg",
+      "https://i.pinimg.com/originals/3b/8e/c7/3b8ec7580ed31dbbb9da7433573614c8.jpg",
+      "https://i.pinimg.com/originals/80/2f/c2/802fc2d81abdd1b6e20f63f589bc967c.jpg",
+      "https://www.bmw.es/content/dam/bmw/common/all-models/m-series/m8-coupe/2022/onepager/bmw-m8-coupe-onepager-sd-bmw-individual-01.jpg",
+      "https://ymimg1.b8cdn.com/resized/car_model/8046/pictures/7420629/listing_main_03.jpg",
+      "https://i.pinimg.com/originals/1f/17/59/1f17599a81f8cc6435ffb653acae9a8d.jpg",
+      "https://i.pinimg.com/originals/fc/e6/05/fce605921fcde3a9457ba14a09b32f11.jpg?nii=t",
+    ],
+    weeklyMileage: [
+      {
+        day: "Mon",
+        km: 12 
+      },
+      {
+        day: "Tue",
+        km: 20 
+      },
+      {
+        day: "Wed",
+        km: 5 
+      },
+      {
+        day: "Thu",
+        km: 23 
+      },
+      {
+        day: "Fri",
+        km: 18 
+      },
+      {
+        day: "Sat",
+        km: 12 
+      },
+      {
+        day: "Sun",
+        km: 40 
+      },
+    ],
+     marketing: {
+      user: "Killian James",
+      averagePrice: "$167,702",
+      marketAverage: "$165,507",
+      excellentImpressionShare: "45%",
+      excellentImpressionShareV: 45,
+      desDioValue1: 72,
+      desDioValue2: 83,
+      desDioValue3: 65,
+      decisionValue: "$811",
+      modelSpend: {
+        src1: dio1,
+        src2: dio2,
+        src3: dio3,
+        blue: "$1,150",
+        red: "$1,060",
+      },
+      spendPerUnit: "$811",
+    },
+  },
+
+     {
+    key: 7,
+    name: "LAMBORGHINI URUS",
+    frmes: [
+      "https://static.vecteezy.com/system/resources/previews/048/028/417/non_2x/lamborghini-urus-suv-on-isolated-transparent-background-png.png",
+      "https://cloud01.topsite.nl/lamborghini.stgallen/car/27/silhouette-menu-2234.png",
+      "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=scale-down,width=1280/sites/default/files/article_images/urus-02.jpg?itok=w1B8CBAh",
+      "https://auto.vercity.ru/gallery/img/automobiles/Lamborghini/2019-11%20Lamborghini%20Urus%20by%20Mansory/900x/1575191923_85.jpg",
+      "https://larte-design.com/storage/app/media/kits/lamborghini/urus-se/coming-soon/renders/grey-lambo-urus-se-facelift-rear.webp",
+      "https://images.dealer.com/ddc/vehicles/2025/Lamborghini/Urus/SUV/trim_SE_59705f/perspective/side-left/2025_76.png",
+      "https://larte-design.ru/storage/app/media/kits/lamborghini/urus-s/colors/grigio-nimbus/lamborgnini-urus-carbon-upgrade-grigio-nimbus.webp",
+      "https://cdna.artstation.com/p/marketplace/presentation_assets/004/213/754/large/file.jpg?1736692421",
+    ],
+    weeklyMileage: [
+      {
+        day: "Mon",
+        km: 26 
+      },
+      {
+        day: "Tue",
+        km: 21 
+      },
+      {
+        day: "Wed",
+        km: 8 
+      },
+      {
+        day: "Thu",
+        km: 17 
+      },
+      {
+        day: "Fri",
+        km: 23 
+      },
+      {
+        day: "Sat",
+        km: 31 
+      },
+      {
+        day: "Sun",
+        km: 29 
+      },
+    ],
+     marketing: {
+      user: "John Junior",
+      averagePrice: "$227,403",
+      marketAverage: "$224,302",
+      excellentImpressionShare: "62%",
+      excellentImpressionShareV: 62,
+      desDioValue1: 84,
+      desDioValue2: 60,
+      desDioValue3: 73,
+      decisionValue: "$908",
+      modelSpend: {
+        src1: dio1,
+        src2: dio2,
+        src3: dio3,
+        blue: "$1,480",
+        red: "$1,290",
+      },
+      spendPerUnit: "$908",
+    },
+  },
+
+     {
+    key: 8,
+    name: "MERCEDES-BENZ AMG GTR",
+    frmes: [
+      "https://i.pinimg.com/originals/20/cc/f1/20ccf199c3a66677eae4f8ce39bc61d7.png",
+      "https://i.pinimg.com/originals/ca/76/de/ca76de0d8929ad3e6e267649c146930c.png",
+      "https://www.renderhub.com/zifir3d/mercedes-benz-amg-gt-coupe-2024/mercedes-benz-amg-gt-coupe-2024-25.jpg",
+      "https://www.renderhub.com/squir/mercedes-amg-gt-r-pro-2020/mercedes-amg-gt-r-pro-2020-05.jpg",
+      "https://www.renderhub.com/zifir3d/mercedes-benz-amg-gt-coupe-2024/mercedes-benz-amg-gt-coupe-2024-07.jpg",
+      "https://www.renderhub.com/lowmod/mercedes-benz-amg-gt-coupe-2024/mercedes-benz-amg-gt-coupe-2024-04.jpg",
+      "https://file.kelleybluebookimages.com/kbb/base/evox/CP/12097/2018-Mercedes-Benz-Mercedes-AMG%20GT-front_12097_032_1822x708_297_cropped.png",
+      "https://assets.meinauto.de/image/upload/q_auto:eco/f_auto/dpr_1.0/c_scale,h_614,w_920/v1/prod/mercedes/amggt/1/2roadster/mercedes_18amggtroadsterrs1e_frontview.png",
+    ],
+    weeklyMileage: [
+      {
+        day: "Mon",
+        km: 27 
+      },
+      {
+        day: "Tue",
+        km: 17 
+      },
+      {
+        day: "Wed",
+        km: 14 
+      },
+      {
+        day: "Thu",
+        km: 17 
+      },
+      {
+        day: "Fri",
+        km: 28 
+      },
+      {
+        day: "Sat",
+        km: 34 
+      },
+      {
+        day: "Sun",
+        km: 31 
+      },
+    ],
+     marketing: {
+      user: "Bill Martin",
+      averagePrice: "$93,873",
+      marketAverage: "$88,508",
+      excellentImpressionShare: "42%",
+      excellentImpressionShareV: 42,
+      desDioValue1: 71,
+      desDioValue2: 77,
+      desDioValue3: 45,
+      decisionValue: "$843",
+      modelSpend: {
+        src1: dio1,
+        src2: dio2,
+        src3: dio3,
+        blue: "$1,020",
+        red: "$1,005",
+      },
+      spendPerUnit: "$843",
+    },
+  },
+
+     {
+    key: 9,
+    name: "AUDI RS6",
+    frmes: [
+      "https://p.turbosquid.com/ts-thumb/no/TUgCLN/B0/3/png/1712139361/1920x1080/fit_q87/c95eb67b70e5b0edb6961678144c45c11b797043/3.jpg",
+      "https://aristocar.be/wp-content/uploads/2019/01/audi_rs6.png",
+      "https://files.hodoor.world/main/f7086efb-a736-4c24-aea2-7b2f0768fd90.png",
+      "https://avatars.mds.yandex.net/get-autoru-vos/5231844/dc97171666d2ee7bdba690bb1a25932a/1200x900",
+      "https://elitecarsale.com/wp-content/uploads/2025/03/snimek-obrazovky-2021-08-20-v-1.webp",
+      "https://i.pinimg.com/736x/6d/b0/f9/6db0f967b8b45b92c2acfea6840f054b.jpg",
+      "https://files.hodoor.world/main/81cdad61-8755-4033-950e-5b6bc079bc0b.png",
+      "https://avatars.mds.yandex.net/get-autoru-vos/17038387/71a7458e4b52ca6c1d456b8dae6fc10f/1200x900",
+    ],
+    weeklyMileage: [
+      {
+        day: "Mon",
+        km: 18 
+      },
+      {
+        day: "Tue",
+        km: 22 
+      },
+      {
+        day: "Wed",
+        km: 11 
+      },
+      {
+        day: "Thu",
+        km: 17 
+      },
+      {
+        day: "Fri",
+        km: 24 
+      },
+      {
+        day: "Sat",
+        km: 16 
+      },
+      {
+        day: "Sun",
+        km: 32 
+      },
+    ],
+     marketing: {
+      user: "Joul Parking",
+      averagePrice: "$137,381",
+      marketAverage: "$132,844",
+      excellentImpressionShare: "58%",
+      excellentImpressionShareV: 58,
+      desDioValue1: 88,
+      desDioValue2: 62,
+      desDioValue3: 89,
+      decisionValue: "$643",
+      modelSpend: {
+        src1: dio1,
+        src2: dio2,
+        src3: dio3,
+        blue: "$1,057",
+        red: "$1,023",
+      },
+      spendPerUnit: "$643",
+    },
+  },
+
+     {
+    key: 10,
+    name: "PORSCHE 911 GT3",
+    frmes: [
+      "https://www.renderhub.com/zifir3d/porsche-911-gt3/porsche-911-gt3-04.jpg",
+      "https://p.turbosquid.com/ts-thumb/tf/l2NR3o/dw/porsche_911_gt3_2022_0011/jpg/1615124778/1920x1080/fit_q87/136e476ee0fc2a69038cbfda71cc90c71a37c38d/porsche_911_gt3_2022_0011.jpg",
+      "https://assets.superhivemarket.com/store/productimage/657860/image/b77bbd813c6770cced49362477da17a7.jpg",
+      "https://p.turbosquid.com/ts-thumb/tf/l2NR3o/vQ/porsche_911_gt3_2022_doors_0004/jpg/1615124813/1920x1080/fit_q87/7bee0a76e49931574cb6006a863cca8b9fddde1b/porsche_911_gt3_2022_doors_0004.jpg",
+      "https://www.renderhub.com/zifir3d/porsche-911-gt3/porsche-911-gt3-09.jpg",
+      "https://bucket.carmodel.com/images/cm-lg/167078_2-1.jpg",
+      "https://www.autogiz.com/product_images/jpeg/Porsche-911-GT3-Touring-2022.jpg",
+      "https://assets.superhivemarket.com/store/productimage/657859/image/9752c942506e259ff4b2cf430e06143e.jpg",
+    ],
+    weeklyMileage: [
+      {
+        day: "Mon",
+        km: 22 
+      },
+      {
+        day: "Tue",
+        km: 15 
+      },
+      {
+        day: "Wed",
+        km: 17 
+      },
+      {
+        day: "Thu",
+        km: 24 
+      },
+      {
+        day: "Fri",
+        km: 29 
+      },
+      {
+        day: "Sat",
+        km: 18 
+      },
+      {
+        day: "Sun",
+        km: 34 
+      },
+    ],
+     marketing: {
+      user: "Brad Tomas",
+      averagePrice: "$182,458",
+      marketAverage: "$181,322",
+      excellentImpressionShare: "38%",
+      excellentImpressionShareV: 38,
+      desDioValue1: 61,
+      desDioValue2: 73,
+      desDioValue3: 47,
+      decisionValue: "$476",
+      modelSpend: {
+        src1: dio1,
+        src2: dio2,
+        src3: dio3,
+        blue: "$832",
+        red: "$654",
+      },
+      spendPerUnit: "$476",
+    },
+  },
+  
 ]
