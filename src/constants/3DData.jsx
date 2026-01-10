66fb2999bd37b77e93264461af81f2b9140fa802
ismoilrz <ismoil4766451155@gmail.com>
2026-01-10 14:32:41 +0300

    data succes

--- src/constants/3DData.jsx
+++ src/constants/3DData.jsx
@@ -324,129 +324,129 @@ export const Data3d = [
   },
      {
     key: 6,
-    name: "BMW M8",
+    name: "BMW M4 G82",
     frmes: [
-      "https://bmwyedek.com/image/catalog/BMW/BMW%20Seriler/Bmw%20M%20Serisi/BMW%20M8%20Coupe%20xDrive%20Competition/bmw_m8_competition_bmwyedek.png",
-      "https://i.pinimg.com/originals/18/8d/18/188d18a54ae795b3242bfe07587964d6.jpg",
-      "https://i.pinimg.com/originals/3b/8e/c7/3b8ec7580ed31dbbb9da7433573614c8.jpg",
-      "https://i.pinimg.com/originals/80/2f/c2/802fc2d81abdd1b6e20f63f589bc967c.jpg",
-      "https://www.bmw.es/content/dam/bmw/common/all-models/m-series/m8-coupe/2022/onepager/bmw-m8-coupe-onepager-sd-bmw-individual-01.jpg",
-      "https://ymimg1.b8cdn.com/resized/car_model/8046/pictures/7420629/listing_main_03.jpg",
-      "https://i.pinimg.com/originals/1f/17/59/1f17599a81f8cc6435ffb653acae9a8d.jpg",
-      "https://i.pinimg.com/originals/fc/e6/05/fce605921fcde3a9457ba14a09b32f11.jpg?nii=t",
+      "https://cdn.botb.com/media/39030/carousel2611.png",
+      "https://static.3d-baza.com/original_models/140621/e06e33dc4ab24cefb3c9b2e5.jpg",
+      "https://bimmer.work/model/img/21hk.png",
+      "https://static.3d-baza.com/original_models/140621/a05c37ac26194ba68b37bb37.jpg",
+      "https://static.3d-baza.com/original_models/140621/accddf35493b450dadeb4e72.jpg",
+      "https://static.3d-baza.com/original_models/140621/f9e556c55d074a96af12c1c6.jpg",
+      "https://res.cloudinary.com/mufautoshow/image/upload/f_auto/v1688638619/moas/news/BMW-M4-Competition-Coupé_Rear.png",
+      "https://content.scalecar.ru/clipart/i92880/U17EF2grvTVaOJ4S-28.jpg",
     ],
     weeklyMileage: [
       {
         day: "Mon",
-        km: 12 
+        km: 7 
       },
       {
         day: "Tue",
-        km: 20 
-      },
-      {
-        day: "Wed",
-        km: 5 
-      },
-      {
-        day: "Thu",
         km: 23 
       },
       {
+        day: "Wed",
+        km: 11
+      },
+      {
+        day: "Thu",
+        km: 18
+      },
+      {
         day: "Fri",
-        km: 18 
+        km: 9 
       },
       {
         day: "Sat",
-        km: 12 
+        km: 10
       },
       {
         day: "Sun",
-        km: 40 
+        km: 24
       },
     ],
      marketing: {
-      user: "Killian James",
-      averagePrice: "$167,702",
-      marketAverage: "$165,507",
-      excellentImpressionShare: "45%",
-      excellentImpressionShareV: 45,
-      desDioValue1: 72,
-      desDioValue2: 83,
-      desDioValue3: 65,
-      decisionValue: "$811",
+      user: "Mike John",
+      averagePrice: "$82,603",
+      marketAverage: "$79,943",
+      excellentImpressionShare: "62%",
+      excellentImpressionShareV: 62,
+      desDioValue1: 78,
+      desDioValue2: 88,
+      desDioValue3: 69,
+      decisionValue: "$941",
       modelSpend: {
         src1: dio1,
         src2: dio2,
         src3: dio3,
-        blue: "$1,150",
-        red: "$1,060",
+        blue: "$1,360",
+        red: "$1,240",
       },
-      spendPerUnit: "$811",
+      spendPerUnit: "$941",
     },
   },
 
      {
     key: 7,
-    name: "LAMBORGHINI URUS",
+    name: "LAMBORGHINI HURACAN",
     frmes: [
-      "https://static.vecteezy.com/system/resources/previews/048/028/417/non_2x/lamborghini-urus-suv-on-isolated-transparent-background-png.png",
-      "https://cloud01.topsite.nl/lamborghini.stgallen/car/27/silhouette-menu-2234.png",
-      "https://www.classicdriver.com/cdn-cgi/image/format=auto,fit=scale-down,width=1280/sites/default/files/article_images/urus-02.jpg?itok=w1B8CBAh",
-      "https://auto.vercity.ru/gallery/img/automobiles/Lamborghini/2019-11%20Lamborghini%20Urus%20by%20Mansory/900x/1575191923_85.jpg",
-      "https://larte-design.com/storage/app/media/kits/lamborghini/urus-se/coming-soon/renders/grey-lambo-urus-se-facelift-rear.webp",
-      "https://images.dealer.com/ddc/vehicles/2025/Lamborghini/Urus/SUV/trim_SE_59705f/perspective/side-left/2025_76.png",
-      "https://larte-design.ru/storage/app/media/kits/lamborghini/urus-s/colors/grigio-nimbus/lamborgnini-urus-carbon-upgrade-grigio-nimbus.webp",
-      "https://cdna.artstation.com/p/marketplace/presentation_assets/004/213/754/large/file.jpg?1736692421",
+      "https://prestigemodelcars.com/cdn/shop/files/slider-1-car.png?v=1651482200",
+      "https://i.pinimg.com/originals/1a/26/4b/1a264b68768ca2d8ef2762098b5d9048.png",
+      "https://i.pinimg.com/originals/be/c3/b6/bec3b61a1563a68b1a53b19f9fc6aa05.png",
+      "https://i.pinimg.com/originals/6f/86/18/6f8618551bd9c2e95e04d38ae14954a8.png",
+      "https://www.rolls-roycemotorcarsphiladelphia.com/imagetag/11789/4/l/New-2020-Lamborghini-Huracan-EVO-Spyder.jpg",
+      "https://www.renderhub.com/squir/lamborghini-huracan-evo-rwd-spyder-2021/lamborghini-huracan-evo-rwd-spyder-2021-05.jpg",
+      "https://i.pinimg.com/originals/f5/4c/92/f54c926865ee1febeb5938b07e517fb7.png",
+      "https://www.renderhub.com/squir/lamborghini-huracan-evo-rwd-spyder-2021/lamborghini-huracan-evo-rwd-spyder-2021-12.jpg",
     ],
     weeklyMileage: [
       {
         day: "Mon",
-        km: 26 
+        km: 13 
       },
       {
         day: "Tue",
-        km: 21 
+        km: 18
       },
       {
         day: "Wed",
-        km: 8 
+        km: 12
       },
       {
         day: "Thu",
-        km: 17 
+        km: 9
       },
       {
         day: "Fri",
-        km: 23 
+        km: 24 
       },
       {
         day: "Sat",
-        km: 31 
+        km: 28 
       },
       {
         day: "Sun",
-        km: 29 
+        km: 17 
       },
     ],
      marketing: {
-      user: "John Junior",
-      averagePrice: "$227,403",
-      marketAverage: "$224,302",
-      excellentImpressionShare: "62%",
-      excellentImpressionShareV: 62,
-      desDioValue1: 84,
-      desDioValue2: 60,
-      desDioValue3: 73,
-      decisionValue: "$908",
+      user: "Jack Brown",
+      averagePrice: "$221,887",
+      marketAverage: "$216,543",
+      excellentImpressionShare: "52%",
+      excellentImpressionShareV: 52,
+      desDioValue1: 68,
+      desDioValue2: 73,
+      desDioValue3: 57,
+      decisionValue: "$837",
       modelSpend: {
         src1: dio1,
         src2: dio2,
         src3: dio3,
-        blue: "$1,480",
-        red: "$1,290",
+        blue: "$1,090",
+        red: "$1,020",
       },
-      spendPerUnit: "$908",
+      spendPerUnit: "$837",
     },
   },
 
